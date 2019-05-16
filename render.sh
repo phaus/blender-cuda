5101f3fc793aeac4f669b3851c47097587fd0aee
Philipp Haussleiter <philipp@haussleiter.de>
2019-05-16 12:43:44 +0200

    updating render.sh

--- render.sh
+++ render.sh
@@ -2,7 +2,7 @@
 
 # it's super important to give the script AFTER the Blender file so that the scene can be set to GPU.
 
-blender -noaudio --background /in/file.blend \
+cd /in && blender -noaudio --background file.blend \
   --python gpurender.py \
   --render-output /out/ --render-anim --frame-start 1 --frame-end 140 \
   --engine CYCLES
